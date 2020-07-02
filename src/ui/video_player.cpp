705cbc6687083a9638cab9299f10770987901f65
niyayu <niyayu@suanzi.ai>
2020-07-02 13:27:53 +0800

    fix(video_player): format video_player

--- src/ui/video_player.cpp
+++ src/ui/video_player.cpp
@@ -1,86 +1,82 @@
 #include "video_player.hpp"
 
-#include "videoframewidget.h"
 #include <QBoxLayout>
+#include <QDebug>
 #include <QPainter>
 #include <QPushButton>
-#include <QDebug>
 #include <QTimer>
 
+#include "videoframewidget.h"
 
-VideoPlayer::VideoPlayer(QWidget *parent)
-    : QWidget(parent)
-{
-    QPalette pal = palette();
-    pal.setColor(QPalette::Background, Qt::transparent);
-    pal.setColor (QPalette::Foreground, Qt::green);
-    setPalette(pal);
+using namespace suanzi;
 
-    pDetectTipWidget_ = new DetectTipWidget(this);
-    pDetectTipWidget_->hide();
-    pRecognizeTipWidget_ = new RecognizeTipWidget();
-    pRecognizeTipWidget_->hide();
+VideoPlayer::VideoPlayer(QWidget *parent) : QWidget(parent) {
+  QPalette pal = palette();
+  pal.setColor(QPalette::Background, Qt::transparent);
+  pal.setColor(QPalette::Foreground, Qt::green);
+  setPalette(pal);
 
-    pCameralReader1_ = new CameraReader(1, this);
-//    pCameralReader0_ = new CameraReader(0, this);
+  detect_tip_widget_ = new DetectTipWidget(this);
+  detect_tip_widget_->hide();
+  recognize_tip_widget_ = new RecognizeTipWidget();
+  recognize_tip_widget_->hide();
 
-    static QThread detectThread;
-    pAliveTask_ = new AliveTask(&detectThread, this);
-    pDetectTask_ = new DetectTask(&detectThread, this);
-    pRecognizeTask_= new RecognzieTask(this);
+  camera_reader_1_ = new CameraReader(1, this);
+  //   camera_reader_0_ = new CameraReader(0, this);
 
-//    connect((const QObject *)pCameralReader0_,
-//                     SIGNAL(txFrame(PingPangBuffer<MmzImage>*)),
-//                    (const QObject *)pAliveTask_,
-//                     SLOT(rxFrame(PingPangBuffer<MmzImage>*)));
+  static QThread detectThread;
+  antispoof_task_ = new AliveTask(&detectThread, this);
+  detect_task_ = new DetectTask(&detectThread, this);
+  recognize_task_ = new RecognzieTask(this);
 
-    connect((const QObject *)pCameralReader1_,
-                     SIGNAL(txFrame(PingPangBuffer<ImagePackage>*)),
-                    (const QObject *)pDetectTask_,
-                     SLOT(rxFrame(PingPangBuffer<ImagePackage>*)));
+  //   connect((const QObject *)camera_reader_0_,
+  //           SIGNAL(txFrame(PingPangBuffer<MmzImage> *)),
+  //           (const QObject *)antispoof_task_,
+  //           SLOT(rxFrame(PingPangBuffer<MmzImage> *)));
 
-//    connect((const QObject *)pDetectTask_,
-//                     SIGNAL(tx_detection_bgr(PingPangBuffer<ImagePackage> *, DetectionFloat)),
-//                    (const QObject *)pRecognizeTask_,
-//                     SLOT(rxFrame(PingPangBuffer<MmzImage> *, DetectionFloat)));
+  connect((const QObject *)camera_reader_1_,
+          SIGNAL(txFrame(PingPangBuffer<ImagePackage> *)),
+          (const QObject *)detect_task_,
+          SLOT(rxFrame(PingPangBuffer<ImagePackage> *)));
 
-//    connect((const QObject *)pDetectTask_,
-//                     SIGNAL(tx_detection_bgr(PingPangBuffer<ImagePackage> *, DetectionFloat)),
-//                    (const QObject *)pRecognizeTask_,
-//                     SLOT(rxFrame(PingPangBuffer<MmzImage> *, DetectionFloat)));
+  //   connect(
+  //       (const QObject *)detect_task_,
+  //       SIGNAL(tx_detection_bgr(PingPangBuffer<ImagePackage> *,
+  //       DetectionFloat)), (const QObject *)recognize_task_,
+  //       SLOT(rxFrame(PingPangBuffer<MmzImage> *, DetectionFloat)));
 
+  //   connect(
+  //       (const QObject *)detect_task_,
+  //       SIGNAL(tx_detection_bgr(PingPangBuffer<ImagePackage> *,
+  //       DetectionFloat)), (const QObject *)recognize_task_,
+  //       SLOT(rxFrame(PingPangBuffer<MmzImage> *, DetectionFloat)));
 
-//    connect((const QObject *)pRecognizeTask_,
-//                     SIGNAL(txResult(Person)),
-//                    (const QObject *)pRecognizeTipWidget_,
-//                     SLOT(rxResult(Person)));
+  //   connect((const QObject *)recognize_task_, SIGNAL(txResult(Person)),
+  //           (const QObject *)recognize_tip_widget_, SLOT(rxResult(Person)));
 
-   connect((const QObject *)pDetectTask_,
-                   SIGNAL(tx_detection_bgr(PingPangBuffer<ImagePackage> *, DetectionFloat)),
-                   (const QObject *)pDetectTipWidget_,
-                    SLOT(rx_result(PingPangBuffer<ImagePackage> *, DetectionFloat)));
+  connect(
+      (const QObject *)detect_task_,
+      SIGNAL(tx_detection_bgr(PingPangBuffer<ImagePackage> *, DetectionFloat)),
+      (const QObject *)detect_tip_widget_,
+      SLOT(rx_result(PingPangBuffer<ImagePackage> *, DetectionFloat)));
 
-    QTimer::singleShot(1, this, SLOT(init_widgets()));
+  QTimer::singleShot(1, this, SLOT(init_widgets()));
 }
 
-VideoPlayer::~VideoPlayer()
-{
+VideoPlayer::~VideoPlayer() {}
 
+void VideoPlayer::paintEvent(QPaintEvent *event) {
+  QPainter painter(this);
+  detect_tip_widget_->paint(&painter);
+  // painter.drawRect(RECOGNIZE_BOX);
 }
-
-void VideoPlayer::paintEvent(QPaintEvent *event)
-{
-    QPainter painter(this);
-    pDetectTipWidget_->paint(&painter);
-    //painter.drawRect(RECOGNIZE_BOX);
-}
-
 
 void VideoPlayer::init_widgets() {
-    pRecognizeTipWidget_->setFixedSize(RECOGNIZE_TIP_BOX.width(), RECOGNIZE_TIP_BOX.height());
-    pRecognizeTipWidget_->move(RECOGNIZE_TIP_BOX.x(), RECOGNIZE_TIP_BOX.y());
-    //pRecognizeTipWidget_->show();
-    /*pDetectTipWidget_->setFixedSize(RECOGNIZE_BOX.width(), RECOGNIZE_BOX.height());
-    pDetectTipWidget_->move(RECOGNIZE_BOX.x(), RECOGNIZE_BOX.y());
-    pDetectTipWidget_->show();*/
+  recognize_tip_widget_->setFixedSize(RECOGNIZE_TIP_BOX.width(),
+                                      RECOGNIZE_TIP_BOX.height());
+  recognize_tip_widget_->move(RECOGNIZE_TIP_BOX.x(), RECOGNIZE_TIP_BOX.y());
+  // recognize_tip_widget_->show();
+  /*detect_tip_widget_->setFixedSize(RECOGNIZE_BOX.width(),
+  RECOGNIZE_BOX.height()); detect_tip_widget_->move(RECOGNIZE_BOX.x(),
+  RECOGNIZE_BOX.y()); detect_tip_widget_->show();*/
 }
--- src/ui/video_player.hpp
+++ src/ui/video_player.hpp
@@ -1,48 +1,51 @@
 #ifndef VIDEOPLAYER_H
 #define VIDEOPLAYER_H
 
-#include <QWidget>
 #include <QScopedPointer>
-#include "pingpangbuffer.h"
-#include "videoframewidget.h"
-#include "detecttipwidget.h"
-#include "recognizetipwidget.h"
+#include <QWidget>
+
+#include "alive_task.h"
 #include "camera_reader.h"
 #include "detect_task.h"
-#include "recognize_task.h"
-#include "alive_task.h"
-#include "image_package.h"
 #include "detection_float.h"
+#include "detecttipwidget.h"
+#include "image_package.h"
+#include "pingpangbuffer.h"
+#include "recognize_task.h"
+#include "recognizetipwidget.h"
+#include "videoframewidget.h"
 
-using namespace suanzi;
+namespace suanzi {
 
-class VideoPlayer : public QWidget
-{
-    Q_OBJECT
+class VideoPlayer : public QWidget {
+  Q_OBJECT
 
-public:
-    VideoPlayer(QWidget *parent = nullptr);
-    ~VideoPlayer();
+ public:
+  VideoPlayer(QWidget *parent = nullptr);
+  ~VideoPlayer();
 
-protected:
-    void paintEvent(QPaintEvent *event) override;
+ protected:
+  void paintEvent(QPaintEvent *event) override;
 
-private slots:
-    void init_widgets();
+ private slots:
+  void init_widgets();
 
-private:
-    VideoFrameWidget *pVideoFrameWidget_;
-    DetectTipWidget *pDetectTipWidget_;
-    RecognizeTipWidget *pRecognizeTipWidget_;
+ private:
+  const QRect RECOGNIZE_BOX = {150, 300, 500, 500};
+  const QRect RECOGNIZE_TIP_BOX = {150, 200, 500, 100};
 
-    CameraReader *pCameralReader0_;
-    CameraReader *pCameralReader1_;
-    AliveTask *pAliveTask_;
-    DetectTask *pDetectTask_;
-    RecognzieTask *pRecognizeTask_;
+  VideoFrameWidget *video_frame_widget_;
+  DetectTipWidget *detect_tip_widget_;
+  RecognizeTipWidget *recognize_tip_widget_;
 
-    const QRect RECOGNIZE_BOX = {150, 300, 500, 500};
-    const QRect RECOGNIZE_TIP_BOX = {150, 200, 500, 100};
+  CameraReader *camera_reader_0_;
+  CameraReader *camera_reader_1_;
+
+  AliveTask *antispoof_task_;
+  DetectTask *detect_task_;
+  RecognzieTask *recognize_task_;
 };
+
+}  // namespace suanzi
 
 #endif
