ad75a4a239ea5da7e3b3497fc4d9559d6ac8868b
niyayu <niyayu@suanzi.ai>
2020-07-03 14:32:11 +0800

    feat(recognize_task): sequence query

--- src/app/recognize_task.cpp
+++ src/app/recognize_task.cpp
@@ -7,18 +7,24 @@
 #include <iostream>
 #include <string>
 
+#include "logger.hpp"
+
 using namespace suanzi;
 
-RecognzieTask::RecognzieTask(QThread *pThread, QObject *parent) {
+RecognzieTask::RecognzieTask(QThread *thread, QObject *parent) {
+  // TODO: global configuration
   face_extractor_ = new suanzi::FaceExtractor("facemodel.bin");
+  face_database_ = new suanzi::FaceDatabase("quface");
 
-  if (pThread == nullptr) {
-    static QThread thread;
-    moveToThread(&thread);
-    thread.start();
+  lost_age_ = 0;
+
+  if (thread == nullptr) {
+    static QThread new_thread;
+    moveToThread(&new_thread);
+    new_thread.start();
   } else {
-    moveToThread(pThread);
-    pThread->start();
+    moveToThread(thread);
+    thread->start();
   }
 }
 
@@ -27,57 +33,118 @@ RecognzieTask::~RecognzieTask() {
 }
 
 void RecognzieTask::rx_frame(PingPangBuffer<ImagePackage> *buffer,
-                            DetectionFloat detection) {
-  ImagePackage *pPang = buffer->get_pang();
-  // printf("RecognzieTask threadId=%x   %x %d\n", QThread::currentThreadId(),
-  // pPang, pPang->frame_idx);
-  //   QThread::msleep(100);
+                             DetectionFloat detection) {
+  ImagePackage *pang = buffer->get_pang();
 
-  suanzi::FaceDetection face_detection;
-  int w = pPang->img_bgr_large->width-1;
-  int h = pPang->img_bgr_large->height-1;
   if (detection.b_valid) {
-    face_detection.bbox.x = detection.x * w;
-    face_detection.bbox.y = detection.y * h;
-    face_detection.bbox.width = detection.width * w;
-    face_detection.bbox.height = detection.height * h;
-    for (int i = 0; i < 5; i++) {
-      face_detection.landmarks.point[i].x = detection.landmark[i][0] * w;
-      face_detection.landmarks.point[i].y = detection.landmark[i][1] * h;
-    }
+    lost_age_ = 0;
+  
+    // crop in large image
+    int width = pang->img_bgr_large->width;
+    int height = pang->img_bgr_large->height;
+    suanzi::FaceDetection face_detection =
+        to_detection(detection, width, height);
 
-    printf("frame:%d  %f %f %f %f \n", detection.frame_idx,
-           face_detection.bbox.x, face_detection.bbox.y,
-           face_detection.bbox.width, face_detection.bbox.height);
-
-    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
-
+    // 25ms
     suanzi::FaceFeature feature;
     face_extractor_->extract(
-        (const SVP_IMAGE_S *)pPang->img_bgr_large->pImplData, face_detection,
+        (const SVP_IMAGE_S *)pang->img_bgr_large->pImplData, face_detection,
         feature);
 
-    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
-    std::chrono::duration<double> time_span =
-        std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
-    std::cout << "extract: "
-              << ": \t" << time_span.count() << "\tseconds." << std::endl;
+    buffer->switch_buffer();
+    emit tx_finish();
 
-    for (int i = 0; i < 10; i++) {
-      printf("%0.3f ", feature.value[i]);
+    // push query result to history
+    std::vector<suanzi::QueryResult> results;
+    if (SZ_RETCODE_OK == face_database_->query(feature, 1, results))
+      history_.push_back(results[0]);
+
+    // deduce recognize results
+    if (history_.size() >= HISTORY_SIZE) {
+      int face_id;
+
+      if (query(history_, face_id)) {
+        SZ_LOG_INFO("recognized: face_id = {}", face_id);
+      } else {
+        SZ_LOG_INFO("recognized: unknown", face_id);
+      }
+
+      history_.clear();
     }
-    printf("\n");
+
+  } else {
+    // no face
+    lost_age_ += 1;
+    if (lost_age_ >= MAX_LOST_AGE) {
+      history_.clear();
+      lost_age_ = 0;
+    }
+  }
+}
+
+suanzi::FaceDetection RecognzieTask::to_detection(
+    DetectionFloat detection_ratio, int width, int height) {
+  suanzi::FaceDetection face_detection;
+  face_detection.bbox.x = detection_ratio.x * width;
+  face_detection.bbox.y = detection_ratio.y * height;
+  face_detection.bbox.width = detection_ratio.width * width;
+  face_detection.bbox.height = detection_ratio.height * height;
+
+  for (int i = 0; i < 5; i++) {
+    face_detection.landmarks.point[i].x =
+        detection_ratio.landmark[i][0] * width;
+    face_detection.landmarks.point[i].y =
+        detection_ratio.landmark[i][1] * height;
   }
 
-  //
-  static bool bInit = false;
-  if (!bInit) {
-    static Person person;
-    person.name = "hahaha";
-    emit tx_result(person);
-    bInit = true;
+  return face_detection;
+}
+
+bool RecognzieTask::query(std::vector<suanzi::QueryResult> history,
+                          int &face_id) {
+  std::map<int, int> person_counts;
+  std::map<int, float> person_accumulate_score;
+  std::map<int, float> person_max_score;
+
+  // initialize map
+  for (auto &person : history_) {
+    person_counts[person.face_id] = 0;
+    person_accumulate_score[person.face_id] = 0.f;
+    person_max_score[person.face_id] = 0.f;
   }
 
-  // end current frame
-  buffer->switch_buffer();
+  // accumulate id and score
+  for (auto &person : history_) {
+    person_counts[person.face_id] += 1;
+    person_accumulate_score[person.face_id] += person.score;
+    person_max_score[person.face_id] =
+        std::max(person.score, person_max_score[person.face_id]);
+  }
+
+  // person id with max counts
+  auto max_person = std::max_element(
+      person_counts.begin(), person_counts.end(),
+      [](const std::pair<int, int> &i, const std::pair<int, int> &j) {
+        return i.second < j.second;
+      });
+
+  int max_person_id = max_person->first;
+  int max_count = max_person->second;
+  float max_person_accumulate_score = person_accumulate_score[max_person_id];
+  float max_person_score = person_max_score[max_person_id];
+
+  if (max_count >= MIN_RECOGNIZE_COUNT &&
+      max_person_accumulate_score >= MIN_ACCUMULATE_SCORE &&
+      max_person_score >= MIN_RECOGNIZE_SCORE) {
+
+    SZ_LOG_DEBUG("id={}, count={}, accumulate_score={}",
+                 max_person_id, max_count,
+                 person_accumulate_score[max_person_id]);
+
+    face_id = max_person_id;
+    return true;
+  }
+  else {
+    return false;
+  }
 }
--- src/app/recognize_task.hpp
+++ src/app/recognize_task.hpp
@@ -5,9 +5,10 @@
 
 #include "detection_float.h"
 #include "image_package.h"
-#include "pingpang_buffer.h"
 #include "person.h"
+#include "pingpang_buffer.h"
 
+#include "quface/db.hpp"
 #include "quface/face.hpp"
 
 namespace suanzi {
@@ -15,18 +16,36 @@ namespace suanzi {
 class RecognzieTask : QObject {
   Q_OBJECT
  public:
-    RecognzieTask(QThread *pThread = nullptr, QObject *parent = nullptr);
-    ~RecognzieTask();
+  RecognzieTask(QThread *thread = nullptr, QObject *parent = nullptr);
+  ~RecognzieTask();
 
  private slots:
-    void rx_frame(PingPangBuffer<ImagePackage> *buffer, DetectionFloat detection);
+  void rx_frame(PingPangBuffer<ImagePackage> *buffer, DetectionFloat detection);
 
  signals:
   void tx_result(Person person);
+  void tx_finish();
 
  private:
-  suanzi::FaceExtractor  *face_extractor_;
+  static constexpr int HISTORY_SIZE = 10;
 
+  static constexpr int MIN_RECOGNIZE_COUNT = 6;
+  static constexpr float MIN_RECOGNIZE_SCORE = 0.65f;
+  static constexpr float MIN_ACCUMULATE_SCORE = 3.6f; // 0.6 * 6;
+
+  static constexpr int MAX_LOST_AGE = 10;
+
+  suanzi::FaceDetection to_detection(DetectionFloat detection_ratio, int width,
+                                     int height);
+
+  bool query(std::vector<suanzi::QueryResult> history, int &face_id);
+
+  suanzi::FaceExtractor *face_extractor_;
+  suanzi::FaceDatabase *face_database_;
+
+  std::vector<suanzi::QueryResult> history_;
+
+  int lost_age_;
 };
 
 }  // namespace suanzi
