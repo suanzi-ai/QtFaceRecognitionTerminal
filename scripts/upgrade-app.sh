03579a94412ec98db0add51175b52abc1289adcd
Xu Zhipei <xuzhipei@gmail.com>
2020-07-29 13:49:22 +0800

    fix(script): check login result

--- scripts/upgrade-app.sh
+++ scripts/upgrade-app.sh
@@ -10,6 +10,12 @@ upgrade_pkg=build/face-terminal.tgz
 body=$(curl -s -d "{\"user\": \"$user\", \"password\": \"$pass\"}" -H "Content-Type: application/json" http://$remote/adm-api/v1/login)
 
 token=$(printf "%s" $body | jq '.token')
+
+if [ "$token" = "null" ]; then
+    printf "Login failed %s" $body | jq '.message'
+    exit 1
+fi
+
 token="${token%\"}"
 token="${token#\"}"
 
