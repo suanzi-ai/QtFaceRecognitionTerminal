7a87e6c5df704b6ba6366a2001e5e783329b9572
Xu Zhipei <xuzhipei@gmail.com>
2020-07-31 11:27:58 +0800

    feat(personService): update person face image api

--- src/service/person_service.cpp
+++ src/service/person_service.cpp
@@ -37,3 +37,105 @@ void suanzi::from_json(const json &j, PersonData &p) {
     SZ_LOG_ERROR("Parse json error {}", exc.what());
   }
 }
+
+using namespace suanzi;
+
+SZ_RETCODE PersonService::get_person(SZ_UINT32 id, PersonData &person) {
+  std::string path = "/api/v1/persons/" + std::to_string(id);
+  auto res = client_.Get(path.c_str());
+  if (res && res->status < 400) {
+    // SZ_LOG_DEBUG("Got person body {}", res->body);
+    json body = json::parse(res->body);
+    body.get_to(person);
+    person.face_path = image_store_path_ + person.face_path;
+    return SZ_RETCODE_OK;
+  }
+  return SZ_RETCODE_FAILED;
+}
+
+SZ_RETCODE PersonService::update_person_face_image(
+    uint id, const std::vector<SZ_UINT8> &image_content) {
+  std::string path = "/api/v1/persons/" + std::to_string(id) + "/faceImage";
+  std::string content(image_content.begin(), image_content.end());
+  httplib::MultipartFormDataItems items = {
+      {"file", content, "avatar.jpg", "images/jpeg"},
+  };
+
+  auto res = client_.Post(path.c_str(), items);
+  if (res && res->status < 400) {
+    // SZ_LOG_DEBUG("Got person body {}", res->body);
+    return SZ_RETCODE_OK;
+  }
+  return SZ_RETCODE_FAILED;
+}
+
+SZ_RETCODE PersonService::report_face_record(
+    uint person_id, const std::vector<SZ_UINT8> &image_content,
+    const std::string &status) {
+  std::string content(image_content.begin(), image_content.end());
+  httplib::MultipartFormDataItems items = {
+      {"file", content, "face.jpg", "images/jpeg"},
+      {"type", "record", "", ""},
+  };
+
+  auto imgRes = client_.Post("/api/v1/images", items);
+  if (!imgRes) {
+    SZ_LOG_ERROR("Upload image failed, no res");
+    return SZ_RETCODE_FAILED;
+  }
+
+  if (imgRes->status >= 400) {
+    SZ_LOG_ERROR("Upload image failed {}", imgRes->body);
+    return SZ_RETCODE_FAILED;
+  }
+
+  std::string filePath;
+  try {
+    json body = json::parse(imgRes->body);
+    filePath = body["filePath"];
+  } catch (std::exception &exc) {
+    SZ_LOG_ERROR("Json parse err {}", exc.what());
+    return SZ_RETCODE_FAILED;
+  }
+
+  json j = {
+      {"personID", person_id},
+      {"imagePath", filePath},
+      {"status", status},
+  };
+  std::string path = "/api/v1/faceRecords";
+  auto res = client_.Post(path.c_str(), j.dump(), "application/json");
+  if (!res) {
+    SZ_LOG_ERROR("Created face record failed, no res");
+    return SZ_RETCODE_FAILED;
+  }
+
+  if (res->status >= 400) {
+    SZ_LOG_ERROR("Create record failed {}", res->body);
+    return SZ_RETCODE_FAILED;
+  }
+  return SZ_RETCODE_OK;
+}
+
+std::string PersonService::get_status(PersonStatus s) {
+  switch (s) {
+    case Normal:
+      return "normal";
+      break;
+    case Blacklist:
+      return "blacklist";
+      break;
+    case Stranger:
+      return "stranger";
+      break;
+    case Fake:
+      return "fake";
+      break;
+    case Clear:
+      return "clear";
+      break;
+    default:
+      return "";
+      break;
+  }
+}
--- src/service/person_service.hpp
+++ src/service/person_service.hpp
@@ -46,89 +46,16 @@ class PersonService {
       : client_(scheme_host_port.c_str()),
         image_store_path_(image_store_path) {}
 
-  SZ_RETCODE get_person(SZ_UINT32 id, PersonData &person) {
-    std::string path = "/api/v1/persons/" + std::to_string(id);
-    auto res = client_.Get(path.c_str());
-    if (res && res->status == 200) {
-      // SZ_LOG_DEBUG("Got person body {}", res->body);
-      json body = json::parse(res->body);
-      body.get_to(person);
-      person.face_path = image_store_path_ + person.face_path;
-      return SZ_RETCODE_OK;
-    }
-    return SZ_RETCODE_FAILED;
-  }
+  SZ_RETCODE get_person(SZ_UINT32 id, PersonData &person);
+
+  SZ_RETCODE update_person_face_image(
+      uint id, const std::vector<SZ_UINT8> &image_content);
 
   SZ_RETCODE report_face_record(uint person_id,
                                 const std::vector<SZ_UINT8> &image_content,
-                                const std::string &status) {
-    std::string content(image_content.begin(), image_content.end());
-    httplib::MultipartFormDataItems items = {
-        {"file", content, "face.jpg", "images/jpeg"},
-        {"type", "record", "", ""},
-    };
+                                const std::string &status);
 
-    auto imgRes = client_.Post("/api/v1/images", items);
-    if (!imgRes) {
-      SZ_LOG_ERROR("Upload image failed, no res");
-      return SZ_RETCODE_FAILED;
-    }
-
-    if (imgRes->status >= 400) {
-      SZ_LOG_ERROR("Upload image failed {}", imgRes->body);
-      return SZ_RETCODE_FAILED;
-    }
-
-    std::string filePath;
-    try {
-      json body = json::parse(imgRes->body);
-      filePath = body["filePath"];
-    } catch (std::exception &exc) {
-      SZ_LOG_ERROR("Json parse err {}", exc.what());
-      return SZ_RETCODE_FAILED;
-    }
-
-    json j = {
-        {"personID", person_id},
-        {"imagePath", filePath},
-        {"status", status},
-    };
-    std::string path = "/api/v1/faceRecords";
-    auto res = client_.Post(path.c_str(), j.dump(), "application/json");
-    if (!res) {
-      SZ_LOG_ERROR("Created face record failed, no res");
-      return SZ_RETCODE_FAILED;
-    }
-
-    if (res->status >= 400) {
-      SZ_LOG_ERROR("Create record failed {}", res->body);
-      return SZ_RETCODE_FAILED;
-    }
-    return SZ_RETCODE_OK;
-  }
-
-  static std::string get_status(PersonStatus s) {
-    switch (s) {
-      case Normal:
-        return "normal";
-        break;
-      case Blacklist:
-        return "blacklist";
-        break;
-      case Stranger:
-        return "stranger";
-        break;
-      case Fake:
-        return "fake";
-        break;
-      case Clear:
-        return "clear";
-        break;
-      default:
-        return "";
-        break;
-    }
-  }
+  static std::string get_status(PersonStatus s);
 
  private:
   httplib::Client2 client_;
