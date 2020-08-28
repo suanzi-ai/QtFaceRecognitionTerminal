09be7baba83995e61e76e9980ae1b6e6324fb9bf
niyayu <niyayu@suanzi.ai>
2020-08-28 13:07:21 +0800

    fix(otpa_task): fix temperature optimazation

--- src/app/temperature/otpa_task.cpp
+++ src/app/temperature/otpa_task.cpp
@@ -19,13 +19,15 @@ void OtpaTask::run() {
     if (otpa16_->read_temperature(&otpa_temp)) {
       temperature = otpa_temp.pixel_temp[otpa_temp.max_pixel_temp_index];
 
-      if (temperature < 35.)
-        temperature = 35. + (temperature - (int)temperature);
-      else if (temperature < 36.)
-        temperature = 36. + (temperature - (int)temperature);
-      // printf("max temp:%.2f\n",
-      //        otpa_temp.pixel_temp[otpa_temp.max_pixel_temp_index]);
-      emit tx_temperature(otpa_temp.pixel_temp[otpa_temp.max_pixel_temp_index]);
+      if (temperature > 30.) {
+        if (temperature < 35.)
+          temperature = 35. + (temperature - (int)temperature);
+        else if (temperature < 36.)
+          temperature = 36. + (temperature - (int)temperature);
+        // printf("max temp:%.2f\n",
+        //        otpa_temp.pixel_temp[otpa_temp.max_pixel_temp_index]);
+      }
+      emit tx_temperature(temperature);
     }
     usleep(250000);
   }
