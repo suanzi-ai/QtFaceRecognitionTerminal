d7a5f0cd14027b65bea4f8b264ca127e5c7e10ed
Yayu Ni <niyayu@suanzi.ai>
2020-10-29 17:49:39 +0800

    feat(outline_widget): draw outline.png

--- src/ui/outline_widget.cpp
+++ src/ui/outline_widget.cpp
@@ -7,7 +7,7 @@
 using namespace suanzi;
 
 OutlineWidget::OutlineWidget(int width, int height, QWidget *parent)
-    : QWidget(parent) {
+    : QWidget(parent), outline_(":asserts/outline.png") {
   QPalette palette = this->palette();
   palette.setColor(QPalette::Background, Qt::transparent);
   setPalette(palette);
@@ -19,28 +19,11 @@ OutlineWidget::OutlineWidget(int width, int height, QWidget *parent)
 OutlineWidget::~OutlineWidget() {}
 
 void OutlineWidget::paint(QPainter *painter) {
-  if (!Config::get_user().enable_temperature)
-    return;
+  if (!Config::get_user().enable_temperature) return;
 
-  return;
-  const int w = width();
-  const int h = height();
+  int w = width();   // 800
+  int h = height();  // 1280
 
-  auto temperature = Config::get_temperature();
-
-  float face_x = temperature.device_face_x * w;
-  float face_y = temperature.device_face_y * h;
-  float face_width = temperature.device_face_width * w;
-  float face_height = temperature.device_face_height * h;
-  float start_angle = temperature.device_body_start_angle;
-  float open_angle = temperature.device_body_open_angle;
-
-  painter->setPen(QPen(Qt::white, 5, Qt::SolidLine));
-  painter->drawEllipse(face_x, face_y, face_width, face_height);
-
-  painter->drawChord(face_x / 1.2,  // 1.2是身体要比头宽一点
-                    face_y + face_height + 25,  // 25是身体与头部的间距
-                    face_width * 1.2,  // 1.2是身体要比头宽一点
-                    face_height, start_angle, open_angle);
-
+  painter->drawPixmap(QRect(0, 0.1875 * h, w, 0.6171875 * h), outline_,
+                      QRect());
 }
