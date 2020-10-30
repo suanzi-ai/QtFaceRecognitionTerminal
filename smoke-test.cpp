d4a5517f93ec848cbab5c40b1723013f2e1ebee3
Xu Zhipei <xuzhipei@gmail.com>
2020-10-30 14:02:46 +0800

    chore(smoke-test): remove unused

--- smoke-test.cpp
+++ smoke-test.cpp
@@ -6,52 +6,14 @@
 #include <string>
 #include <thread>
 #include <vector>
+
 #include "config.hpp"
 #include "mpi_sys.h"
 
 using namespace suanzi;
 using namespace suanzi::io;
 
-LCDScreenType load_screen_type() {
-  std::string conf_filename = "/userdata/user.conf";
-  std::ifstream conf(conf_filename);
-  if (!conf.is_open()) {
-    SZ_LOG_ERROR("Can't open {}", conf_filename);
-    return (LCDScreenType)-1;
-  }
-
-  std::string line;
-  std::regex reg("^LCD=(\\d+).+", std::regex_constants::ECMAScript |
-                                      std::regex_constants::icase);
-  std::smatch matches;
-  while (std::getline(conf, line)) {
-    if (std::regex_match(line, matches, reg)) {
-      if (matches.size() == 2) {
-        std::ssub_match base_sub_match = matches[1];
-        std::string lcd_type = base_sub_match.str();
-        // 0: mipi-7inch-1024x600(default)
-        // 1: mipi-8inch-800x1280
-        // 2: mipi-10inch-800x1280
-        // 4: mipi-5inch-480x854
-
-        if (lcd_type == "1") {
-          SZ_LOG_INFO("Load screen type SML_LCD_MIPI_8INCH_800X1280");
-          return SML_LCD_MIPI_8INCH_800X1280;
-        } else if (lcd_type == "4") {
-          SZ_LOG_INFO("Load screen type SML_LCD_MIPI_5INCH_480X854");
-          return SML_LCD_MIPI_5INCH_480X854;
-        } else {
-          SZ_LOG_ERROR("lcd type unknown {}", lcd_type);
-        }
-      }
-    }
-  }
-
-  return (LCDScreenType)-1;
-}
-
 void test() {
-  //auto screen_type = load_screen_type();
   LCDScreenType screen_type = RH_9881_8INCH_800X1280;
   if (!Config::load_screen_type(screen_type)) return;
 
@@ -59,11 +21,10 @@ void test() {
   SensorType sensor1_type = SONY_IMX327_2L_MIPI_2M_30FPS_12BIT;
   if (!Config::load_sensor_type(sensor0_type, sensor1_type)) return;
 
-  
   EngineOption opt = {
       .bgr =
           {
-          	  .sensor_type = sensor0_type,
+              .sensor_type = sensor0_type,
               .dev = 0,
               .flip = true,
               .channels =
@@ -81,7 +42,7 @@ void test() {
           },
       .nir =
           {
-          	  .sensor_type = sensor1_type,
+              .sensor_type = sensor1_type,
               .dev = 1,
               .flip = true,
               .channels =
